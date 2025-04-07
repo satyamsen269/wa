4e8dbd26a376a43d4498f2cbc4b9572aa115da92
satyamsen269 <satyamsen269@gmail.com>
2025-04-07 10:23:27 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'wa',
+  host: 'sql111.infinityfree.com',
+  user: 'if0_36942383',
+  password: 'hMs26FhQx5n',
+  database: 'if0_36942383_wa',
 });
 
 module.exports = pool;
